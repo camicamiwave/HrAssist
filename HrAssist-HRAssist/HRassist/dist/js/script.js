0bcc4ecc1173917671257e1267d5a276513c6b28
Frankenstein <gadildonitarose@gmail.com>
2023-11-12 12:58:17 +0800

    update

--- HrAssist-HRAssist/HRassist/dist/js/script.js
+++ HrAssist-HRAssist/HRassist/dist/js/script.js
@@ -5,51 +5,51 @@ const submenuItems = document.querySelectorAll(".submenu_item");
 const sidebarOpen = document.querySelector("#sidebarOpen");
 const sidebarClose = document.querySelector(".collapse_sidebar");
 const sidebarExpand = document.querySelector(".expand_sidebar");
-sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
+const mainContent = document.querySelector("#main");
+
+sidebarOpen.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    mainContent.classList.toggle("close");
+});
 
 sidebarClose.addEventListener("click", () => {
     sidebar.classList.add("close", "hoverable");
+    mainContent.classList.add("close");
 });
 
 sidebarExpand.addEventListener("click", () => {
     sidebar.classList.remove("close", "hoverable");
+    sidebar.classList.add("expand");
+    mainContent.classList.remove("close");
 });
 
 sidebar.addEventListener("mouseenter", () => {
     if (sidebar.classList.contains("hoverable")) {
         sidebar.classList.remove("close");
+        mainContent.classList.remove("close");
     }
 });
 
 sidebar.addEventListener("mouseleave", () => {
-    if (sidebar.classList.contains("hoverable")) {
+    if (sidebar.classList.contains("hoverable") && !sidebar.classList.contains("expand")) {
         sidebar.classList.add("close");
+        mainContent.classList.add("close");
     }
 });
 
 darkLight.addEventListener("click", () => {
     body.classList.toggle("dark");
     if (body.classList.contains("dark")) {
-        document.setI
         darkLight.classList.replace("bx-sun", "bx-moon");
     } else {
         darkLight.classList.replace("bx-moon", "bx-sun");
     }
 });
 
-submenuItems.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("show_submenu");
-        submenuItems.forEach((item2, index2) => {
-        if (index !== index2) {
-            item2.classList.remove("show_submenu");
-        }
-        });
-    });
-});
-
 if (window.innerWidth < 768) {
     sidebar.classList.add("close");
+    mainContent.classList.add("close");
 } else {
     sidebar.classList.remove("close");
+    mainContent.classList.remove("close");
 }
