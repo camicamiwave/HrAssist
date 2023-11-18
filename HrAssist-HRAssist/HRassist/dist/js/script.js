5a04b702e27190d6e5e3c1e58bc1876e7ba2bdc9
Frankenstein <gadildonitarose@gmail.com>
2023-11-19 00:28:43 +0800

    up

--- HrAssist-HRAssist/HRassist/dist/js/script.js
+++ HrAssist-HRAssist/HRassist/dist/js/script.js
@@ -55,6 +55,18 @@ if (window.innerWidth < 768) {
 }
 
 
+let uploadButton = document.getElementById("upload-button");
+let chosenImage = document.getElementById("chosen-image");
+let fileName = document.getElementById("file-name");
+
+uploadButton.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    reader.onload = () => {
+        chosenImage.setAttribute("src",reader.result);
+    }
+    fileName.textContent = uploadButton.files[0].name;
+}
                      /**
    * Prompt request form
    */
