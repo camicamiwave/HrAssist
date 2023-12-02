5d41f97584e290b0abc3e4beafcaab7bdadf754c
riyuwin <albosjohn0@gmail.com>
2023-12-03 02:55:53 +0800

    Wip

--- HrAssist-HRAssist/HRassist/webpack.config.js
+++ HrAssist-HRAssist/HRassist/webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
         manage_employee: './src/manage_employee.js',
         rewards: './src/rewards.js',
         applicant_status: './src/applicant_status.js',
-        leave_management: './src/leave_management.js'
+        leave_management: './src/leave_management.js',
+        ipcrf: './src/ipcrf.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist/bundles'),
