fe5b7ea6d418ec4986ea2cfe5b763fc7f94c1b4f
riyuwin <albosjohn0@gmail.com>
2023-12-04 13:38:59 +0800

    201 FIle WIP

--- HrAssist-HRAssist/HRassist/webpack.config.js
+++ HrAssist-HRAssist/HRassist/webpack.config.js
@@ -42,7 +42,9 @@ module.exports = {
         ipcrf_list: './src/ipcrf_list.js',
         individual_ipcrf: './src/individual_ipcrf.js',
         retrieve_pds: './src/retrieve_pds.js', 
-        navigator201: './src/201navigator.js'
+        navigator201: './src/201navigator.js',
+        retrieve_appointment: './src/retrieve_appointment.js',
+        retrieve_attachments: './src/retrieve_attachments.js'
         
     },
     output: {
